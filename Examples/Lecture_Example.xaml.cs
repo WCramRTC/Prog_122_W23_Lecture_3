a7e43b604bfaf796f3c129cd62d46a4d89e2e8e0
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-01-11 13:22:23 -0800

    Initial commit

---
+++ Examples/Lecture_Example.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prog_122_W23_Lecture_3
+{
+    /// <summary>
+    /// Interaction logic for Lecture_Example.xaml
+    /// </summary>
+    public partial class Lecture_Example : Window
+    {
+        public Lecture_Example()
+        {
+            InitializeComponent();
+        }
+
+        // Appends the first name to rtbDisplay
+        private void btnFName_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text += txtFName.Text;
+        }
+
+        private void btnLastName_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text += txtLName.Text;
+        }
+
+        private void btnAddFields_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text += txtFName.Text + " " + txtLName.Text;
+
+        }
+
+        private void btnDisplayFLName_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text = txtFName.Text + " " + txtLName.Text;
+
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text = "";
+        }
+
+        private void btnNewLine_Click(object sender, RoutedEventArgs e)
+        {
+            rtbDisplay.Text += "\n";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Prog_122_W23_Lecture_3
+ Programming 122 - W23 - RichTextBox and Layout Notes
